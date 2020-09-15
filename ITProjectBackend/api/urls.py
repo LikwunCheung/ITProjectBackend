61cd0fae88fe054cd5a9f317a00a24defbb93738
zhanglihuan <lihuanz@student.unimelb.edu.au>
2020-09-15 22:57:55 +0800

    update dockerfile

--- ITProjectBackend/api/urls.py
+++ ITProjectBackend/api/urls.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('v1/', include('ITProjectBackend.api.urls_v1')),
 ]
